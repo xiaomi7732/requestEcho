661efd2da970f8e6e8d0414714a9da3ec924d2e1
Saar Shen 🚾 <saars@microsoft.com>
2020-10-16 15:17:17 -0700

    Update the controller to accept extension calls

--- Controllers/EchoController.cs
+++ Controllers/EchoController.cs
@@ -14,8 +14,21 @@ namespace Microsoft.RequestEcho
         [Route("subscriptions/{subscriptionId}/providers/Microsoft.Profiler/stampToken")]
         public IActionResult Get(string subscriptionId)
         {
-            string echoContent = JsonConvert.SerializeObject(Request.Headers);
+            string echoContent = GetEchoContent();
             return Ok(subscriptionId + ":" + echoContent);
+        }
+
+        private string GetEchoContent()
+        {
+            return JsonConvert.SerializeObject(Request.Headers);
+        }
+
+        [HttpGet]
+        [Route("subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/microsoft.insights/components/{componentName}/providers/microsoft.profiler/stampToken")]
+        public IActionResult Get(string subscriptionId, string resourceGroupName, string componentName)
+        {
+            string echoContent = GetEchoContent();
+            return Ok(componentName + ":" + echoContent);
         }
     }
 }
